2a165cd1594395a020cb2a2e15cab73053e33db8
Vasiliy Makarov <feitgraph@gmail.com>
2019-05-04 13:34:31 +0300

    annotation in repository

--- src/main/java/ru/nvg/printservice/dao/JobRepository.java
+++ src/main/java/ru/nvg/printservice/dao/JobRepository.java
@@ -2,8 +2,10 @@ package ru.nvg.printservice.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 import ru.nvg.printservice.domain.Job;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job, Long>,
         QuerydslPredicateExecutor<Job> {
 }
